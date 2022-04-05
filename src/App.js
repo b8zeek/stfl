95e9c2fabc5defa684e0c76ce9b9c219f2be6e71
Basic Mirko <bejzik8@gmail.com>
2022-04-05 19:18:13 +0100

    Prettier configured, repository reformated.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 function App() {
-  return (
-    <div className='App'>
-      <h1>123</h1>
-    </div>
-  )
+    return (
+        <div className='App'>
+            <h1>123</h1>
+        </div>
+    )
 }
 
 export default App
